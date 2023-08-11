5a6d12125598aa1db8e580d36c38560168912463
RobbiAdam <robbyeadam@gmail.com>
2023-08-11 16:52:18 +0700

    update

---
+++ Equals==/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Cat cat= new Cat();
+        Cat cat1= new Cat();
+        Dog dog  = new Dog();
+
+        
+        Console.WriteLine(cat1.equals(cat1));
+        // Console.WriteLine(cat1.equals(dog)); //Cannot convert dog to cat
+    }
+}
+
+class Cat
+{
+    int _legs;
+
+    public int GetLegs()
+    {
+        return _legs;
+    }
+
+    public bool equals(Cat x)
+    {
+        if(x == null || this.GetType() != x.GetType())
+        {
+            return false;
+        }
+        
+        Cat other = (Cat)x;
+        return _legs == other._legs;
+    }
+}
+
+class Dog
+{
+    int _legs;
+
+    public int GetLegs()
+    {
+        return _legs;
+    }
+
+    public bool equals(Dog x)
+    {
+        if(x == null || this.GetType() != x.GetType())
+        {
+            return false;
+        }
+        
+        Dog other = (Dog)x;
+        return _legs == ((Dog)x)._legs;
+    }
+}
--- zDump/Program.cs
+++ zDump/Program.cs
@@ -1,79 +1,8 @@
-﻿interface IFoobar
+﻿class Program
 {
-    void FooBar(int x);
-}
-class Program
-{
-    static void Main()
+    public static void Main(string[] args)
     {
-        Formula formula = new();
-        int _userInput = InputHandler.GetInput();
-        formula.FooBar(_userInput);
+
     }
 }
-static class InputHandler
-{
-    public static int GetInput()
-    {
-        while (true)
-        {
-            Console.Write("Enter a number: ");
-            string _input = Console.ReadLine();
 
-            if (int.TryParse(_input, out int number))
-            {
-                return number;
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number !");
-            }
-
-        }
-    }
-}
-public class Formula : IFoobar
-{
-    public void FooBar(int x)
-    {
-        for (int i = 0; i <= x; i++)
-        {
-            string _result = FindFooBar(i);
-            Console.WriteLine(_result);
-        }
-    }
-    // private int FindFoobar(int x)
-    private string FindFooBar(int x)
-    {
-        int _foo = 3;
-        int _bar = 5;
-
-        if (DivisibleByBoth(x))
-        {
-            // Console.WriteLine("Foobar");
-            return "Foobar";
-        }
-        if (DivisibleBy(_foo, x))
-        {
-            // Console.WriteLine("Foo");
-            return "Foo";
-        }
-        if (DivisibleBy(_bar, x))
-        {
-            // Console.WriteLine("Bar");
-            return "Bar";
-        }
-        // return x
-        return x.ToString();
-    }
-
-    private bool DivisibleBy(int x, int number)
-    {
-        return number % x == 0;
-    }
-
-    private bool DivisibleByBoth(int number)
-    {
-        return DivisibleBy(3, number) && DivisibleBy(5, number);
-    }
-}
